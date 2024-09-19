78ef547f01a1531dc003580241782c3b7e5f72aa
sallen12 <sam.allen@stat.unibe.ch>
2024-09-19 15:13:55 +0200

    tidy simulation studies

--- scripts/example.R
+++ scripts/example.R
@@ -12,11 +12,6 @@ x_n1 <- seq(-3, 3, 0.75)
 
 fit <- conformal_idr(x = x, y = y, x_out = x_n1, y_out = NA, online = FALSE)
 
-#fit$points[1] <- -5
-#fit$points[n + 2] <- 5
-#fit$cdf_lcnf[1, ] <- 0
-#fit$cdf_ucnf[n + 2, ] <- 1
-
 df <- data.frame(x = fit$points,
                  y = c(as.vector(fit$cdf_oos), as.vector(fit$cdf_ucnf), as.vector(fit$cdf_lcnf)),
                  b = rep(c("Crisp CDF", "Conformal bounds", "Conformal bound2"), each = 9*length(fit$points)),
@@ -24,10 +19,12 @@ df <- data.frame(x = fit$points,
 ggplot(df) + geom_step(aes(x = x, y = y, col = b)) + facet_wrap(vars(xn)) +
   scale_x_continuous(name = "Threshold", limits = c(-5, 5)) +
   scale_y_continuous(name = "CDF") +
-  scale_color_manual(values = c("Conformal bounds" = "#F8766D", "Conformal bound2" = "#F8766D", "Crisp CDF" = "#619CFF"),
+  scale_color_manual(values = c("Conformal bounds" = "#F8766D",
+                                "Conformal bound2" = "#F8766D",
+                                "Crisp CDF" = "#619CFF"),
                      breaks = c("Conformal bounds", "Crisp CDF")) +
   theme_bw() +
   theme(legend.title = element_blank(),
         legend.position = "bottom")
-ggsave("plots/simstudy_norm.png", width = 8, height = 6)
 
+ggsave("plots/simstudy_norm.png", width = 8, height = 6)
--- scripts/sim_study.R
+++ scripts/sim_study.R
@@ -6,27 +6,11 @@ set.seed(90743892)
 library(scoringRules)
 library(WeightedForecastVerification)
 library(ggplot2)
-library(isodistrreg)
-library(Rcpp)
-sourceCpp("lspm_cpp.cpp")
 
-source("online_idr.R")
-sourceCpp("online_idr_computation_weights.cpp")
+source("scripts/utility_funcs.R")
 
-source("utility_funcs.R")
-
-eval_ideal <- function(preds, obs, anti = FALSE) {
-  if (anti) {
-    pit <- pgamma(obs, shape = sqrt(10 - preds), scale = pmin(pmax(10 - preds, 1), 6))
-    crps <- crps_gamma(obs, shape = sqrt(10 - preds), scale = pmin(pmax(10 - preds, 1), 6))
-  } else {
-    pit <- pgamma(obs, shape = sqrt(preds), scale = pmin(pmax(preds, 1), 6))
-    crps <- crps_gamma(obs, shape = sqrt(preds), scale = pmin(pmax(preds, 1), 6))
-  }
-  return(list(pit = pit, crps = crps))
-}
-
-get_results <- function(N_tr = 500, x_ts, y_ts, anti = FALSE) {
+# wrapper to fit and evaluate the different prediction methods
+get_results <- function(N_tr = 500, x_ts, y_ts, anti = FALSE, k = 10) {
   start <- Sys.time()
 
   x_tr <- runif(N_tr, 0, 10)
@@ -34,9 +18,9 @@ get_results <- function(N_tr = 500, x_ts, y_ts, anti = FALSE) {
   if (anti) x_tr <- 10 - x_tr
 
   N_ts <- length(y_ts)
-  pit <- crps <- data.frame(replicate(4, numeric(N_ts)))
-  colnames(pit) <- colnames(crps) <- c("id", "lspm", "cidr", "mond")
-  thicc <- pit
+  pit <- data.frame(replicate(3, numeric(N_ts)))
+  colnames(pit) <- c("lspm", "cidr", "mond")
+  thicc <- crps <- pit
 
   t_vec <- unname(quantile(y_ts, c(0.1, 0.25, 0.5, 0.75, 0.9)))
   n_t <- length(t_vec)
@@ -44,15 +28,8 @@ get_results <- function(N_tr = 500, x_ts, y_ts, anti = FALSE) {
               cidr = matrix(NA, N_ts, n_t),
               mond = matrix(NA, N_ts, n_t))
 
-  ### Ideal
-  scores <- eval_ideal(x_ts, y_ts, anti = anti)
-  pit[['id']] <- scores$pit
-  crps[['id']] <- scores$crps
-  thicc[['id']] <- NA
-
   ### LSPM
-  lspm_preds <- fit_lspm(y = y_tr, X = as.matrix(x_tr), X_ts = as.matrix(x_ts))
-  #lspm_preds <- fit_olspm(y = y_tr, X = as.matrix(x_tr), X_ts = as.matrix(x_ts))
+  lspm_preds <- fit_lspm(y = y_tr, X = x_tr, X_ts = x_ts)
   scores <- eval_lspm(lspm_preds, y_ts, t_vec)
   pit[['lspm']] <- scores$pit
   crps[['lspm']] <- scores$crps
@@ -68,7 +45,6 @@ get_results <- function(N_tr = 500, x_ts, y_ts, anti = FALSE) {
   thicc[['cidr']] <- scores$thick
 
   ### Mondrian
-  k <- 10
   mond_preds <- fit_mond(y = y_tr, X = x_tr, X_ts = x_ts, k)
   scores <- eval_mond(mond_preds, y_ts, t_vec)
   pit[['mond']] <- scores$pit
@@ -78,11 +54,13 @@ get_results <- function(N_tr = 500, x_ts, y_ts, anti = FALSE) {
 
   end <- Sys.time()
   print(end - start)
+
   return(list(pit = pit, crps = crps, F_t = F_t, thick = thicc))
 }
 
-plot_results <- function(pit, crps, F_t = NULL, obs = NULL, type = "rel_diag", filename = NULL) {
-  if (type == "hist") {
+# wrapper to plot and save PIT histograms, pp-plots, and threshold calibration plots
+plot_cal <- function(pit, crps, F_t = NULL, obs = NULL, type = "pitpp", filename = NULL) {
+  if (type == "pithist") {
     ## PIT histograms
     id_plot <- pit_hist(pit[['id']], ranks = F, ymax = 0.4, xlab = NULL, xticks = F,
                         title = paste("Ideal: CRPS =", round(mean(crps[['id']], na.rm = T), 3)))
@@ -92,32 +70,32 @@ plot_results <- function(pit, crps, F_t = NULL, obs = NULL, type = "rel_diag", f
                           title = paste("CIDR: CRPS =", round(mean(crps[['cidr']], na.rm = T), 3)))
     mond_plot <- pit_hist(pit[['mond']], ranks = F, ymax = 0.4, xlab = NULL, xticks = F,
                           title = paste("LB: CRPS =", round(mean(crps[['mond']], na.rm = T), 3)))
-    pit_plot <- gridExtra::grid.arrange(lspm_plot, cidr_plot, mond_plot, ncol = 1)
-  } else if (type == "threshold") {
-    t_vec <- quantile(obs, c(0.1, 0.25, 0.5, 0.75, 0.9))
-    ## threshold calibration
-    lspm_plot <- threshreldiag(F_t[['lspm']], obs, t = t_vec, title = "LSPM")
-    cidr_plot <- threshreldiag(F_t[['cidr']], obs, t = t_vec, title = "CIDR")
-    mond_plot <- threshreldiag(F_t[['mond']], obs, t = t_vec, title = "LB")
-    pit_plot <- gridExtra::grid.arrange(lspm_plot, cidr_plot, mond_plot, nrow = 1)
-  } else {
-    ## PIT reliability diagrams
+    cal_plot <- gridExtra::grid.arrange(lspm_plot, cidr_plot, mond_plot, ncol = 1)
+  } else if (type == "pitpp") {
+    ## PIT pp-plots
     id_plot <- pit_reldiag(pit[['id']], title = paste("Ideal: CRPS =", round(mean(crps[['id']], na.rm = T), 3)))
     lspm_plot <- pit_reldiag(pit[['lspm']], title = paste("LSPM: CRPS =", round(mean(crps[['lspm']], na.rm = T), 3)))
     cidr_plot <- pit_reldiag(pit[['cidr']], title = paste("CIDR: CRPS =", round(mean(crps[['cidr']], na.rm = T), 3)))
     mond_plot <- pit_reldiag(pit[['mond']], title = paste("LB: CRPS =", round(mean(crps[['mond']], na.rm = T), 3)))
-    pit_plot <- gridExtra::grid.arrange(lspm_plot, cidr_plot, mond_plot, ncol = 1)
+    cal_plot <- gridExtra::grid.arrange(lspm_plot, cidr_plot, mond_plot, ncol = 1)
+  } else if (type == "threshcal") {
+    ## threshold calibration
+    t_vec <- quantile(obs, c(0.1, 0.25, 0.5, 0.75, 0.9))
+    lspm_plot <- threshreldiag(F_t[['lspm']], obs, t_vec, title = "LSPM")
+    cidr_plot <- threshreldiag(F_t[['cidr']], obs, t_vec, title = "CIDR")
+    mond_plot <- threshreldiag(F_t[['mond']], obs, t_vec, title = "LB")
+    cal_plot <- gridExtra::grid.arrange(lspm_plot, cidr_plot, mond_plot, nrow = 1)
+  } else {
+    stop("argument 'type' must be one of 'pithist', 'pitpp', and 'threshcal'")
   }
 
   if (!is.null(filename)) {
-    if (type == "threshold") {
-      ggsave(plot = pit_plot, filename, width = 10.5, height = 3.5, dpi = 300)
+    if (type == "threshcal") {
+      ggsave(plot = cal_plot, filename, width = 10.5, height = 3.5, dpi = 300)
     } else {
-      ggsave(plot = pit_plot, filename, width = 2.5, height = 7.5, dpi = 300)
+      ggsave(plot = cal_plot, filename, width = 2.5, height = 7.5, dpi = 300)
     }
 
-  } else {
-    return(pit_plot)
   }
 }
 
@@ -127,8 +105,11 @@ plot_results <- function(pit, crps, F_t = NULL, obs = NULL, type = "rel_diag", f
 
 N_ts <- 5000
 x_ts <- runif(N_ts, 0, 10)
-#x_ts <- rbeta(N_ts, shape1 = 2, shape2 = 5)*10
+
+# isotonic example
 y_ts <- rgamma(N_ts, shape = sqrt(x_ts), scale = pmin(pmax(x_ts, 1), 6))
+
+# antitonic example
 y_ts_at <- rgamma(N_ts, shape = sqrt(10 - x_ts), scale = pmin(pmax(10 - x_ts, 1), 6))
 
 df <- data.frame(x = x_ts[1:1000], y = y_ts[1:1000])
@@ -137,7 +118,7 @@ ggplot(df) + geom_point(aes(x = x, y = y), size = 0.5) +
   scale_y_continuous(name = "Y", limits = c(0, 80)) +
   theme_bw() +
   theme(panel.grid = element_blank())
-#ggsave("simstudy_data.png", width = 3, height = 3)
+ggsave("plots/simstudy_data.png", width = 3, height = 3)
 
 
 ################################################################################
@@ -154,13 +135,13 @@ res_2000 <- get_results(N_tr = 2000, x_ts, y_ts) # 5 mins
 ##### evaluate
 
 ## probabilistic calibration
-plot_results(res_100$pit, res_100$crps, filename = "simstudy_100.png")
-plot_results(res_500$pit, res_500$crps, filename = "simstudy_500.png")
-plot_results(res_1000$pit, res_1000$crps, filename = "simstudy_1000.png")
-plot_results(res_2000$pit, res_2000$crps, filename = "simstudy_2000.png")
+plot_cal(res_100$pit, res_100$crps, filename = "plots/simstudy_100.png")
+plot_cal(res_500$pit, res_500$crps, filename = "plots/simstudy_500.png")
+plot_cal(res_1000$pit, res_1000$crps, filename = "plots/simstudy_1000.png")
+plot_cal(res_2000$pit, res_2000$crps, filename = "plots/simstudy_2000.png")
 
 ## threshold calibration
-plot_results(res_2000$pit, res_2000$crps, res_2000$F_t, y_ts, type = "threshold", filename = "simstudy_2000_tc.png")
+plot_cal(F_t = res_2000$F_t, obs = y_ts, type = "threshcal", filename = "plots/simstudy_2000_tc.png")
 
 
 ################################################################################
@@ -177,13 +158,13 @@ res_2000_at <- get_results(N_tr = 2000, x_ts, y_ts_at, anti = T) # 5 mins
 ##### evaluate
 
 ## probabilistic calibration
-plot_results(res_100_at$pit, res_100_at$crps, filename = "simstudy_100_at.png")
-plot_results(res_500_at$pit, res_500_at$crps, filename = "simstudy_500_at.png")
-plot_results(res_1000_at$pit, res_1000_at$crps, filename = "simstudy_1000_at.png")
-plot_results(res_2000_at$pit, res_2000_at$crps, filename = "simstudy_2000_at.png")
+plot_cal(res_100_at$pit, res_100_at$crps, filename = "plots/simstudy_100_at.png")
+plot_cal(res_500_at$pit, res_500_at$crps, filename = "plots/simstudy_500_at.png")
+plot_cal(res_1000_at$pit, res_1000_at$crps, filename = "plots/simstudy_1000_at.png")
+plot_cal(res_2000_at$pit, res_2000_at$crps, filename = "plots/simstudy_2000_at.png")
 
 ## threshold calibration
-plot_results(res_2000_at$pit, res_2000_at$crps, res_2000_at$F_t, y_ts_at, type = "threshold", filename = "simstudy_2000_tc_at.png")
+plot_cal(F_t = res_2000_at$F_t, obs = y_ts_at, type = "threshcal", filename = "plots/simstudy_2000_tc_at.png")
 
 
 ##### compare thicknesses
@@ -194,4 +175,4 @@ ggplot(df) + geom_boxplot(aes(x = mode, y = thicc)) +
   scale_y_continuous(name = "Thickness") +
   theme_bw() +
   theme(panel.grid = element_blank())
-ggsave("simstudy_thick.png", width = 3, height = 3)
+ggsave("plots/simstudy_thick.png", width = 3, height = 3)
